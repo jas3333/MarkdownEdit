2eb3aec4908f6824b10036a53b74a8a4773c9c54
jas3333 <accounts1@protonmail.com>
2022-12-07 20:24:53 -0800

    First commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,28 @@
+import { useState } from 'react';
+import MarkDown from 'react-markdown';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+
 function App() {
-    return <div className='App'></div>;
+    const [text, setText] = useState('');
+
+    return (
+        <div className='container'>
+            <div className='container mg-left-lg mg-top-lg'>
+                <form>
+                    <textarea
+                        rows='40'
+                        cols='80'
+                        className='bg-mid'
+                        value={text}
+                        onChange={(event) => setText(event.target.value)}
+                    />
+                </form>
+            </div>
+            <div className='container-col mg-left-lg mg-top-lg'>
+                <ReactMarkdown>{text}</ReactMarkdown>
+            </div>
+        </div>
+    );
 }
 
 export default App;
